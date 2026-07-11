8fc62ce4e17c07bacdb55dd400d57c7eb766b9f3
Sandip Dutta <duttasandip11100@gmail.com>
2026-07-11 16:09:59 +0530

    Time: 3 ms (87.84%) | Memory: 52.6 MB (22.34%) - LeetSync

--- leetcode-sync/200-number-of-islands/number-of-islands.java
+++ leetcode-sync/200-number-of-islands/number-of-islands.java
@@ -1,27 +1,36 @@
 class Solution {
-    private static final int[][] DIRS = {{1,0},{-1,0},{0,1},{0,-1}};
+    private static final char LAND = '1';
+    private static final char WATER = '0';
+
+    private void sink(char[][] grid, int r, int c) {
+        final int nRows = grid.length;
+        final int nCols = grid[0].length;
+
+        if (r >= 0 && r < nRows && c >= 0 && c < nCols) {
+            if (grid[r][c] == WATER)
+                return;
+            grid[r][c] = WATER;
+            sink(grid, r, c + 1);
+            sink(grid, r, c - 1);
+            sink(grid, r + 1, c);
+            sink(grid, r - 1, c);
+        }
+    }
 
     public int numIslands(char[][] grid) {
-        int rows = grid.length, cols = grid[0].length, count = 0;
-        Queue<int[]> queue = new ArrayDeque<>();   // ArrayDeque > LinkedList
+        final int nRows = grid.length;
+        final int nCols = grid[0].length;
+        int numIslands = 0;
 
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                if (grid[r][c] == '1') {
-                    count++;
-                    grid[r][c] = '0';               // mark seed AT enqueue
-                    queue.add(new int[]{r, c});
-                    while (!queue.isEmpty()) {
-                        int[] cell = queue.remove();
-                        for (int[] d : DIRS) {
-                            int x = cell[0] + d[0], y = cell[1] + d[1];
-                            if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == '1') {
-                                grid[x][y] = '0';   // mark neighbor AT enqueue
-                                queue.add(new int[]{x, y});
-                            }
-                        }
-                    }
+        for (int r = 0; r < nRows; r++) {
+            for (int c = 0; c < nCols; c++) {
+                if (grid[r][c] == LAND) {
+                    numIslands++;
+                    sink(grid, r, c);
                 }
-        return count;
+            }
+        }
+
+        return numIslands;
     }
 }
