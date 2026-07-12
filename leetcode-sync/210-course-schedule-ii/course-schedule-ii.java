f91403903ca9ef487a6447a48e0e983a5ae5fc21
Sandip Dutta <duttasandip11100@gmail.com>
2026-07-12 20:51:15 +0530

    Time: 6 ms (43.77%) | Memory: 46.7 MB (67.16%) - LeetSync

--- leetcode-sync/210-course-schedule-ii/course-schedule-ii.java
+++ leetcode-sync/210-course-schedule-ii/course-schedule-ii.java
@@ -1,35 +1,60 @@
 class Solution {
+    private static final int UNVISITED = 0;
+    private static final int VISITED = 1;
+    private static final int DONE = 2;
+    private boolean hasCycle = false;
+
+    private void dfs(
+            int node,
+            List<List<Integer>> adj,
+            List<Integer> finalOrder,
+            int[] nodeStatus) {
+        if (nodeStatus[node] == VISITED) {
+            hasCycle = true;
+            return;
+        }
+
+        if (nodeStatus[node] == DONE) return;
+
+        nodeStatus[node] = VISITED;
+
+        for (int nb : adj.get(node)) {
+            dfs(nb, adj, finalOrder, nodeStatus);
+            if (hasCycle)
+                return;
+        }
+
+        if (nodeStatus[node] != DONE) {
+            finalOrder.add(node);
+            nodeStatus[node] = DONE;
+        }
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>(numCourses);
-        List<Integer> coursesOrder = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++)
+        List<Integer> finalOrder = new ArrayList<>();
+        int[] nodeStatus = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
-
-        int[] inDegree = new int[numCourses];
-        for (int[] pre : prerequisites) {
-            adj.get(pre[1]).add(pre[0]); // prereq -> course
-            inDegree[pre[0]]++; // count in same pass
         }
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < numCourses; i++)
-            if (inDegree[i] == 0)
-                queue.add(i);
-
-        int completed = 0;
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-            coursesOrder.add(node);
-            completed++;
-            for (int next : adj.get(node))
-                if (--inDegree[next] == 0)
-                    queue.add(next);
+        for (int[] edge : prerequisites) {
+            int source = edge[1];
+            int destination = edge[0];
+            adj.get(source).add(destination);
         }
 
-        if (completed == numCourses) {
-            return coursesOrder.stream().mapToInt(i -> i).toArray();
-        } else {
-            return new int[0];
+        for (int node = 0; node < numCourses; node++) {
+            if (nodeStatus[node] == UNVISITED) {
+                dfs(node, adj, finalOrder, nodeStatus);
+            }
         }
+
+        Collections.reverse(finalOrder);
+
+        return !hasCycle
+                ? finalOrder.stream().mapToInt(Integer::intValue).toArray()
+                : new int[0];
     }
 }
